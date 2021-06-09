25dc9ca292ab3ad9b30e1b9b157a044c9b92ba64
Moumita Ghosh <ghoshm259@gmail.com>
2021-06-09 14:47:38 +0530

    initial

--- lib/Variable.dart
+++ lib/Variable.dart
@@ -1,14 +1,14 @@
 //SOCIAL MEDIA LINKS
 const String MAIL_ID = "ghoshm259@gmail.com";
-const String LINKEDLN_URL = "linkedin.com/in/soumen-pal-9a355b154";
-const String GITHUB_URL = "https://github.com/soumen744488";
-const String TWITTER_URL = "https://twitter.com/palsoumen748";
-const String INSTAGRAM_URL = "https://www.instagram.com/soumen748";
+const String LINKEDLN_URL = "https://www.linkedin.com/in/moumita-ghosh-48071a1b2/";
+const String GITHUB_URL = "https://github.com/Moumita-ghosh05021998";
+const String TWITTER_URL = "";
+const String INSTAGRAM_URL = "";
 const String FACEBOOK_URL = "https://fb.com/";
 
 
 //INTRODUCTION PAGE VARIABLE
-const String NAME = "Soumen Pal";
+const String NAME = "Moumita Ghosh";
 const String PROFESSION_DETAILS = 'Software Developer and Final Year Computer Science Engineering Student';
 const String MORE_DETAILS_INTRODUCTION = "I love to build neat, cool and scalable application and I'm an aspiring Full Stack engineer.\n"
     'I love to learn and build new stuff that are beneficial to the community and cool to work on.\n'
@@ -19,28 +19,28 @@ const String MORE_DETAILS_INTRODUCTION = "I love to build neat, cool and scalabl
 //IMAGE ASSET PATH
 const String IMAGE_PATH = 'assets/images/moumita.jpg';
 
-//ABOUT ME SECTION
-const String WHO_AM_I ="Hello! I'm Soumen Pal  , an Computer Science and Engineering students and software Developer.\n"
+//ABOUT ME SECT
+const String WHO_AM_I ="Hello! I'm Moumita Ghosh  , an Computer Science and Engineering students and software Developer.\n"
     'I love to create performant and interesting stuff that are benificial to the community. I enjoy learning and exploring new areas in the technologies. \nIn my opinion,'
     'getting your hands dirty is the best way to learn a new skill.\n'
     'My area of interest are Cross Platform Application Development , Android Application Development and Full Stack Development\n'
-    'Beside Programming, I love playing cricket and watching cricket matches with a mug full of coffee.';
+    'Beside Programming, I love collect information about verious dance style.';
 
 //EDUCATION
-const String DEGREE = "Bachelor of Technology in Computer Science & Engineering ( 2017-2021 ) ";
-const String COLLEGE_NAME = "Saroj Mohan Institute Of Technology";
-const String CGPA = "CGPA :- 7.3";
+const String DEGREE = "Bachelor of Technology in Computer Science & Engineering ( 2018-2021 ) ";
+const String COLLEGE_NAME = "Government College Of Engineering and Textile Technology, Serampore ";
+const String CGPA = "CGPA :- 8.7";
 
-const String TWELVETH_SCHOOL = "HOOMGARH CHANDABILA HIGH SCHOOL";
-const String TWELVETH_PERSENTAGE = "Percentage :- 64";
+const String TWELVETH_SCHOOL = "Panjipukur Smt. Tulsi Devi Smriti Vidyapith";
+const String TWELVETH_PERSENTAGE = "Percentage :- 70.2";
 
-const String TENTH_SCHOOL = "AGARBANDH HIGH SCHOOL";
-const String TENTH_PERSENTAGE = "Percentage :- 60";
+const String TENTH_SCHOOL = "Harit High School";
+const String TENTH_PERSENTAGE = "Percentage :- 67.57";
 
 
 //SKILLS
 const String SKILL1 = "JAVA ( Programming & Android )";
-const String SKILL2 = "PYTHON ( Programming )";
+const String SKILL2 = "SQL";
 const String SKILL3 = "FLUTTER ( DART )" ;
 const String SKILL4 = "GIT" ;
 const String SKILL5 = "SQLite" ;
